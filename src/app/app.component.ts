6f52be6fd9e23b21d8b0bb2a4520cfb592c4c4a7
giseleMM <giselemedina01@gmail.com>
2024-04-11 12:14:32 -0300

    clase1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clase_1';
+  protected buffer = '';
+  protected onBTN() {
+    this.title = this.buffer;
+  }
+  protected edadUno = 0;
+  protected edadDos = 0;
+  protected promedio = 0;
+  protected onCalcular() {
+    console.log(this.edadUno);
+    console.log(this.edadDos);
+    this.promedio = (this.edadUno + this.edadDos) / 2.0;
+
+  }
+  protected onLimpiar() {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+  }
+  ngOnInit() {
+    // alert("inicio");
+  }
 }
